287e72369f0be1fb5729185df5c1095447698fbe
Jan Hovland <jan.hovland@lyse.net>
2021-02-14 10:29:52 +0100

    Initial Commit

---
+++ Shared/ArticlesApp.swift
@@ -0,0 +1,20 @@
+//
+//  ArticlesApp.swift
+//  Shared
+//
+//  Created by Jan Hovland on 14/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct ArticlesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
